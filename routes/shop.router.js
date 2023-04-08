b66d097a30b296f974da8b2badce73bf51528ab0
Ajay <ajayzyan100@gmail.com>
2023-04-08 09:00:15 +0530

    view customisation halfdone

--- routes/shop.router.js
+++ routes/shop.router.js
@@ -4,7 +4,7 @@ const shopRouter = express.Router();
 const shopController = require('../controllers/shop.controller');
 
 shopRouter.get('/dd',(req,res)=>{
-    res.render('shop/home.ejs');
+    res.render('shop/logins/signup.login.ejs');
 })
 
 module.exports = shopRouter;
